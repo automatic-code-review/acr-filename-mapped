5cd885028f6fb7d6f9a2b01a083ea23b2f567a9b
Ângelo Hank <hankangelo6@gmail.com>
2024-05-06 19:37:46 -0300

    feat: verificando se o arquivo está mapeado no google sheets

--- src/review.py
+++ src/review.py
@@ -1,3 +1,16 @@
+import gspread
+import gc
+import hashlib
+import json
+
+def getMigrationList():
+    sheet = gc.open(config['sheet'])
+    worksheet = sheet.worksheet(config['worksheet'])
+    migrationList = worksheet.col_values(config['columnOfMigrationName'])
+    migrationList.pop(0)
+
+    return migrationList
+
 def review(config):
     path_target = config['path_target']
     path_source = config['path_source']
@@ -7,18 +20,26 @@ def review(config):
     merge_request_id = merge['merge_request_id']
     
     comments = []
-    
-    # TODO IMPLEMENTAR EXTENSION
-    #  O OBJETO DE COMENTARIO DEVE POSSUIR O SEGUINTE FORMATO
-    #  {
-    #      "id": "",
-    #      "comment": "",
-    #      "position": {
-    #          "language": "",
-    #          "path': "",
-    #          "startInLine": 0,
-    #          "endInLine": 0
-    #    }
-    #  }
+
+    gc = gspread.oauth()
+    gspread.oauth_from_dict(config['credentials'], config['authorizedUser'])
+
+    if path_source not in getMigrationList():
+        comments.append({
+                        "id": __generate_md5(path_source),
+                        "comment": f"Arquivo {path_source} nao mapeado na tabela de migrations",
+                        "position": {
+                            "language": "sql",
+                            "path": path_source,
+                            "startInLine": 1,
+                            "endInLine": 1,
+                            "snipset": False
+                        }
+                    })
 
     return comments
+
+def __generate_md5(string):
+    md5_hash = hashlib.md5()
+    md5_hash.update(string.encode('utf-8'))
+    return md5_hash.hexdigest()
